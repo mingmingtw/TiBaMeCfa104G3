92e3420abf2f29a8a3dc7063562f58c174bd4e87
mingmingtw <encored98931@gmail.com>
2022-01-26 20:53:06 +0800

    1/26更改test

--- CFA104G3/src/main/java/faq/model/TestFaqDAO.java
+++ CFA104G3/src/main/java/faq/model/TestFaqDAO.java
@@ -1,84 +1,68 @@
 package faq.model;
 
-
-
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.sql.Date;
-import java.sql.Timestamp;
 import java.util.List;
 
-import org.junit.After;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import faq.model.FaqVO;
 
-
-// 一個方法對應一個test
 public class TestFaqDAO {
 	private FaqDAO dao;
-	
-	// 建立物件
+
 	@BeforeEach
 	public void before() {
 		dao = new FaqDAO();
 	}
 
-	// 清除
-	@After
+	@AfterEach
 	public void after() {
 		dao = null;
 	}
-	
-	// 注意命名方式大小寫
+
 	@Test
 	public void testInsert() {
 		FaqVO pojo = new FaqVO();
-		
-		// TODO 填入資料, 注意FK資料是否存在
-		pojo.setId(5);
-		pojo.setQuestion("安安");
-		pojo.setAnswer("好醜");
-		
-		
+
+		pojo.setQuestion("如何註冊會員?");
+		pojo.setAnswer("點選畫面右上角頭像進行註冊");
+
 		int row = dao.insert(pojo);
 		assertNotEquals(row, 0);
 	}
-	
+
 	@Test
-	public void testdeleteById() {
-		// TODO 刪除後別忘記還原
-		int row = dao.deleteById(4);
-		assertNotEquals(row, 4);
+	public void testDeleteById() {
+		int row = dao.deleteById(1);
+		assertNotEquals(row, 0);
 	}
-	
+
 	@Test
 	public void testUpdate() {
-		// TODO 請記得填入vo資料
 		FaqVO pojo = new FaqVO();
-		
-		// TODO 填入資料, 注意PK、FK資料是否存在
-		pojo.setId(5);
-		pojo.setQuestion("安安");
-		pojo.setAnswer("好醜");
-		
+
+		pojo.setId(1);
+		pojo.setQuestion("如何登入會員?");
+		pojo.setAnswer("點選畫面右上角頭像登入");
+
 		int row = dao.update(pojo);
-		assertNotEquals(row, 5);
+		assertNotEquals(row, 0);
 	}
-	
+
 	@Test
-	public void testselectById() {
+	public void testSelectById() {
 		FaqVO vo = dao.selectById(1);
 		assertNotNull(vo);
 	}
-	
+
 	@Test
 	public void testSelectAll() {
 		List<FaqVO> vos = dao.selectAll();
 		assertTrue(vos.size() > 0);
 	}
 }
-
-
--- CFA104G3/src/main/java/news/model/TestNewsDAO.java
+++ CFA104G3/src/main/java/news/model/TestNewsDAO.java
@@ -1,79 +1,70 @@
 package news.model;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.List;
 
-import org.junit.After;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-// 一個方法對應一個test
+import news.model.NewsVO;
+
 public class TestNewsDAO {
 	private NewsDAO dao;
 
-	// 建立物件
 	@BeforeEach
 	public void before() {
 		dao = new NewsDAO();
 	}
 
-	// 清除
-	@After
+	@AfterEach
 	public void after() {
 		dao = null;
 	}
 
-	// 注意命名方式大小寫
 	@Test
 	public void testInsert() {
 		NewsVO pojo = new NewsVO();
-		
-		// TODO 填入資料, 注意FK資料是否存在
-		pojo.setId(1);
-		pojo.setContent("安安安安安安安安安安安");
-		pojo.setImage(null);
+
+		byte[] data = new byte[1];
+		data[0] = 0x05;
+		pojo.setContent("最新消息內容");
+		pojo.setImage(data);
 		pojo.setDate(Timestamp.valueOf("2022-10-12 15:00:06"));
 		pojo.setType(1);
-				
-				
-			
-				
-		
-		
+
 		int row = dao.insert(pojo);
 		assertNotEquals(row, 0);
 	}
 
 	@Test
-	public void testdeleteById() {
-		// TODO 刪除後別忘記還原
-		int row = dao.deleteById(2);
+	public void testDeleteById() {
+		int row = dao.deleteById(1);
 		assertNotEquals(row, 0);
 	}
 
 	@Test
 	public void testUpdate() {
-		// TODO 請記得填入vo資料
 		NewsVO pojo = new NewsVO();
-		
-		// TODO 填入資料, 注意PK、FK資料是否存在
-	
+
+		byte[] data = new byte[1];
+		data[0] = 0x06;
 		pojo.setId(1);
-		pojo.setContent("安安安安安安安安安安安");
-		pojo.setImage(null);
+		pojo.setContent("最新消息內容");
+		pojo.setImage(data);
 		pojo.setDate(Timestamp.valueOf("2022-10-12 15:00:06"));
 		pojo.setType(1);
+
 		int row = dao.update(pojo);
-		assertNotEquals(row, 4);
+		assertNotEquals(row, 0);
 	}
 
 	@Test
-	public void testselectById() {
+	public void testSelectById() {
 		NewsVO vo = dao.selectById(1);
 		assertNotNull(vo);
 	}
@@ -81,6 +72,6 @@ public class TestNewsDAO {
 	@Test
 	public void testSelectAll() {
 		List<NewsVO> vos = dao.selectAll();
-		assertTrue(vos.size()  >1);
+		assertTrue(vos.size() > 0);
 	}
 }
